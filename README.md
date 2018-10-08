e288cbbe8c5edfce0949c31ea82f9481a61a1c93
chenrq335 <1015099078@qq.com>
2018-10-08 14:10:59 +0800

    readme.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# proxx6
-proxx6
+# 這是java調用方式
+```java
+public static void main(String[] args) {
+		for(int i=0;i<100;i++){
+			System.out.println(i);
+		}
+	}
+```
--- src/com/szy/services/Test.java
+++ src/com/szy/services/Test.java
@@ -6,8 +6,9 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("chenrq Test");
+		for(int i=0;i<100;i++){
+			System.out.println(i);
+		}
 	}
 
 }
